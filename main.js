a9018b4a5587c1859fa0950b621dc6bb6e821ea5
Karina Zhuro <kzhuro36@gmail.com>
2022-05-10 11:19:34 +0300

    feat: pressing a key on a physical keyboard highlights the key on the virtual keyboard

--- main.js
+++ main.js
@@ -19,31 +19,101 @@ function createKeyboard() {
   const WRAPPER = document.querySelector('.wrapper');
   const KEYBOARD = document.createElement('div');
   
-  const keyboardElem = [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace'],
-    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
-    ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
-    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Up', 'Shift'],
-    ['Ctr', 'Win', 'Alt', 'Space', 'Alt', 'Left', 'Down', 'Right', 'Ctr']
-  ];
+  const keyboardElems = {
+    row1: {
+      Backquote: '`',
+      Digit1: '1',
+      Digit2: '2',
+      Digit3: '3',
+      Digit4: '4',
+      Digit5: '5',
+      Digit6: '6',
+      Digit7: '7',
+      Digit8: '8',
+      Digit9: '9',
+      Digit0: '0',
+      Minus: '-',
+      Equal: '=',
+      Backspace: 'Backspace',
+    },
+    row2: {
+      Tab: 'Tab',
+      KeyQ: 'q',
+      KeyW: 'w',
+      KeyE: 'e',
+      KeyR: 'r',
+      KeyT: 't',
+      KeyY: 'y',
+      KeyU: 'u',
+      KeyI: 'i',
+      KeyO: 'o',
+      KeyP: 'p',
+      BracketLeft: '[',
+      BracketRight: ']',
+      Backslash: '\\',
+      Delete: 'Delete',
+    },
+    row3: {
+      CapsLock: 'CapsLock',
+      KeyA: 'a',
+      KeyS: 's',
+      KeyD: 'd',
+      KeyF: 'f',
+      KeyG: 'g',
+      KeyH: 'h',
+      KeyJ: 'j',
+      KeyK: 'k',
+      KeyL: 'l',
+      Semicolon: ';',
+      Quote: "'",
+      Enter: 'Enter'
+    },
+    row4: {
+      ShiftLeft: 'Shift',
+      KeyZ: 'z',
+      KeyX: 'x',
+      KeyC: 'c',
+      KeyV: 'v',
+      KeyB: 'b',
+      KeyN: 'n',
+      KeyM: 'm',
+      Comma: ',',
+      Period: '.',
+      Slash: '/',
+      ArrowUp: 'ArrowUp',
+      ShiftRight: 'Shift',
+    },
+    row5: {
+      ControlLeft: 'Control',
+      MetaRight: 'Meta',
+      AltLeft: 'Alt',
+      Space: ' ',
+      AltRight: 'Alt',
+      ArrowLeft: 'ArrowLeft',
+      ArrowDown: 'ArrowDown',
+      ArrowRight: 'ArrowRight',
+      ControlRight: 'Control',
+    }
+  };
   
   KEYBOARD.classList.add('keyboard');
   WRAPPER.append(KEYBOARD);
   
-  keyboardElem.forEach(itemRow => {
+  for (let key in keyboardElems) {
     const ROW = document.createElement('div');
     
     ROW.classList.add('row');
     KEYBOARD.append(ROW);
     
-    itemRow.forEach(item => {
+    for (let item in keyboardElems[key]) {
       const BTN = document.createElement('button');
       
-      BTN.textContent = item;
+      BTN.textContent = keyboardElems[key][item];
       BTN.classList.add('key');
+      BTN.classList.add(item);
       ROW.append(BTN);
-    });
-  });
+    }
+  }
 }
 
 function createExtraInfo() {
@@ -55,9 +125,22 @@ function createExtraInfo() {
   WRAPPER.append(INFO);
 }
 
+function setHighlightsBtn(e) {
+  const BTN = document.querySelector(`.${e.code}`);
+  BTN.classList.add('key-highlight');
+}
+
+function unSetHighlightsBtn(e) {
+  const BTN = document.querySelector(`.${e.code}`);
+  BTN.classList.remove('key-highlight');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   createWrap();
   createTextarea();
   createKeyboard();
   createExtraInfo();
 });
+
+document.addEventListener('keydown', setHighlightsBtn);
+document.addEventListener('keyup', unSetHighlightsBtn);
